8af217b43571f85b1fd863dd81ac56e865a8a37c
manuelmenama <manuel.mena090589@gmail.com>
2022-10-20 15:52:10 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log('Loaded "Campo Minato"');
+
+//1 creare html funzionale 
+//2 generare dinamicamente le caselle
+//3 quando clicchiamo sulla casella questa si colora + messaggio in console con numero casella
+//4 creare bottone di start
+//5 creare un una select con tre livelli di difficoltà (1 - 100(10), 2 - 81(9), 3 - 49(7))
